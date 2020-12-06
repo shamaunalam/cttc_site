95f7822f3c8c912151eb1974b2eb81321b1f0be4
Shamaun <shamaunalam@gmail.com>
2020-12-06 22:12:22 +0530

    second commit

--- Idea_1/settings.py
+++ Idea_1/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'PublicSite'
+    'PublicSite',
+    'virtualclass.apps.VirtualclassConfig'
 ]
 
 MIDDLEWARE = [
--- virtualclass/views.py
+++ virtualclass/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def vchome(request):
+    return render(request,'ppt.html')
